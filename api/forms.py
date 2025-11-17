83d0e80a18412728e6dd9307022e81c074155fc5
MANUCON1 <manucontregar@hotmail.com>
2025-11-17 13:24:08 +0100

    Corregir import en forms.py: cambiar de models a .models

--- api/forms.py
+++ api/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from models import User
+from .models import User
 
 class RegisterForm(FlaskForm):
     username = StringField('Nombre de usuario', 
